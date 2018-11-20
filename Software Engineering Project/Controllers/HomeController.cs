e54d6450788f2951a78bd9edb2aa46a0b7d744e6
psestepa <psestepa@gmail.com>
2018-11-19 22:37:18 -0600

    Fixed Login

--- Software Engineering Project/Controllers/HomeController.cs
+++ Software Engineering Project/Controllers/HomeController.cs
@@ -34,7 +34,36 @@ namespace Software_Engineering_Project.Controllers
             return View();
         }
 
-       
+        [HttpPost] //important for logging in
+        public ActionResult Authorize(Software_Engineering_Project.Models.Account userModel)
+        {
+            using (portaldatabaseEntities db = new portaldatabaseEntities())
+            {
+                var userDetails = db.Accounts.Where(x => x.Username == userModel.Username && x.Password == userModel.Password).FirstOrDefault();
+                var account_status = db.Accounts.Where(x => x.Username == userModel.Username && x.Password == userModel.Password && x.StatusID == 1).FirstOrDefault();
+
+                if (userDetails != null) //Correct username/password
+                {
+                    if (account_status != null) //check if status is good
+                    {
+                        Session["Account ID"] = userDetails.AccountID;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        userModel.LoginErrorMessage = "The Account is blocked/deactivated";
+                        return View("Login", userModel);
+                    }
+                }
+                else
+                {
+                    userModel.LoginErrorMessage = "Wrong Username/Password";
+                    return View("Login", userModel);
+                }
+
+            }
+        }
+
 
     }
 }
