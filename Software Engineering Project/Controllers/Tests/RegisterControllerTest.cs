19cbf3411b8dc03c22d0568ee9f7543ce5432b57
psestepa <psestepa@gmail.com>
2018-11-29 15:58:58 -0600

    commit

---
+++ Software Engineering Project/Controllers/Tests/RegisterControllerTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Software_Engineering_Project.Controllers;
+
+namespace Tests.Controllers
+{
+    [TestClass]
+    public class RegisterControllerTest
+    {
+        [TestMethod]
+        public void TestDetailsView()
+        {
+            var controller = new RegisterController();
+            var result = controller.Index() as ActionResult;
+            Assert.AreEqual(result, result);
+
+        }
+    }
+}
